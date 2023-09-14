116e7d8b93c048fd3f656963086b1610491ddcfa
Mrowr Purr <mrowr.purr@gmail.com>
2023-09-14 15:28:38 -0700

    Revert "Revert "Hmmm.""
    This reverts commit 47e00a73866dc8cbe05894fb297ca245528bb262.

--- Scripts/Bindings/BindExamples.sh
+++ Scripts/Bindings/BindExamples.sh
@@ -1,18 +1,18 @@
 # Reminder: deploy this file to your mod folder for testing ;)
 
 # Explicit bindings
-BindMeToPlayer $Player
-BindMeToHodByID 0x1348A
-BindMeToHodByEditorID HodRef
-BindMeToGeneratedQuest $Quest
-BindMeToGeneratedQuestWithName $Quest(MyCoolQuest)
+# BindMeToPlayer $Player
+# BindMeToHodByID 0x1348A
+# BindMeToHodByEditorID HodRef
+# BindMeToGeneratedQuest $Quest
+# BindMeToGeneratedQuestWithName $Quest(MyCoolQuest)
 BindMeToGeneratedObject $Object
 BindMeToGeneratedObjectEditorID $Object(FoodSweetroll)
 BindMeToGeneratedObjectFormID $Object(0x2E4FF)
-BindMeToGenObjectEditorIDWithEditorID $Object(FoodSweetroll,MySweetRoll)
-BindMeToGenObjectFormIDWithEditorID $Object(0x2E4FF,MyGrandSoulGem)
+# BindMeToGenObjectEditorIDWithEditorID $Object(FoodSweetroll,MySweetRoll)
+# BindMeToGenObjectFormIDWithEditorID $Object(0x2E4FF,MyGrandSoulGem)
 
 # Auto
-AutoBindToGeneratedQuest
+# AutoBindToGeneratedQuest
 AutoBindToGeneratedObject
-AutoBindToPlayer
+# AutoBindToPlayer
